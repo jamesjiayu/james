35557dd08977623969673df9679462b7f8dcb237
James Wang <jamesjiayu@gmail.com>
2023-05-14 18:15:37 -0500

    fixing routes

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
-import React, { useState, useEffect } from "react";
-import Preloader from "../src/components/Pre";
-import Navbar from "./components/Navbar";
-import Home from "./components/Home/Home";
-import About from "./components/About/About";
-import Projects from "./components/Projects/Projects";
-import Footer from "./components/Footer";
-import Resume from "./components/Resume/ResumeNew";
+import React, { useState, useEffect } from "react"
+import Preloader from "../src/components/Pre"
+import Navbar from "./components/Navbar"
+import Home from "./components/Home/Home"
+import About from "./components/About/About"
+import Projects from "./components/Projects/Projects"
+import Footer from "./components/Footer"
+import Resume from "./components/Resume/ResumeNew"
 import {
   BrowserRouter as Router,
   Route,
   Routes,
   Navigate
-} from "react-router-dom";
-import ScrollToTop from "./components/ScrollToTop";
-import "./style.css";
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+} from "react-router-dom"
+import ScrollToTop from "./components/ScrollToTop"
+import "./style.css"
+import "./App.css"
+import "bootstrap/dist/css/bootstrap.min.css"
 
-function App() {
-  const [load, upadateLoad] = useState(true);
+function App () {
+  const [load, upadateLoad] = useState(true)
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      upadateLoad(false);
-    }, 1200);
+      upadateLoad(false)
+    }, 1200)
 
-    return () => clearTimeout(timer);
-  }, []);
+    return () => clearTimeout(timer)
+  }, [])
 
   return (
     <Router>
@@ -35,16 +35,16 @@ function App() {
         <Navbar />
         <ScrollToTop />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/project" element={<Projects />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/resume" element={<Resume />} />
-          <Route path="*" element={<Navigate to="/"/>} />
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/project" element={<Projects />} />
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/resume" element={<Resume />} />
+          <Route exact path="*" element={<Navigate to="/" />} />
         </Routes>
         <Footer />
       </div>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
